5f209b44af85f75f46908fac4e1cad69c849ce0c
Fatemeh Farmahini <m_farmahini@yahoo.com>
2017-06-01 11:30:11 +0200

    create class AlternativRepository.java

--- QuizzToolBackend/src/main/java/com/quizztool/quizztoolbackend/repositories/QuestionRepository.java
+++ QuizzToolBackend/src/main/java/com/quizztool/quizztoolbackend/repositories/QuestionRepository.java
@@ -1,14 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.quizztool.quizztoolbackend.repositories;
 
-/**
- *
- * @author Delt604
- */
+import com.quizztool.quizztoolbackend.models.Question;
+import java.util.List;
+import org.hibernate.Session;
+
+
 public class QuestionRepository {
+    public List<Question> getQuizzes() {
+        Session session = HibernateUtil.getSession();
+        List<Question> questions = session.createCriteria(Question.class).list();
+        session.close();
+        return questions;
+    }
     
 }

9d3c80b8dc3b2d6b0091cb3dbf3c982931e37a98
FilipRickardsson <filip.rickardsson.90@gmail.com>
2017-06-03 17:17:35 +0200

    Function to submit a Quizz is working

--- QuizzToolBackend/src/main/java/com/quizztool/quizztoolbackend/repositories/QuizzRepository.java
+++ QuizzToolBackend/src/main/java/com/quizztool/quizztoolbackend/repositories/QuizzRepository.java
@@ -2,14 +2,27 @@ package com.quizztool.quizztoolbackend.repositories;
 
 import com.quizztool.quizztoolbackend.models.Quizz;
 import java.util.List;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
 public class QuizzRepository {
-    
+
     public List<Quizz> getQuizzes() {
         Session session = HibernateUtil.getSession();
         List<Quizz> quizzes = session.createCriteria(Quizz.class).list();
         session.close();
         return quizzes;
+    }
+
+    public Quizz getQuizz(int quizzId) {
+        Session session = HibernateUtil.getSession();
+
+        Criteria criteria = session.createCriteria(Quizz.class);
+        criteria.add(Restrictions.eq("quizzId", quizzId));
+        Quizz quizz = (Quizz) criteria.uniqueResult();
+
+        session.close();
+        return quizz;
     }
 }
--- QuizzToolBackend/src/main/java/com/quizztool/quizztoolbackend/services/QuizzResultService.java
+++ QuizzToolBackend/src/main/java/com/quizztool/quizztoolbackend/services/QuizzResultService.java
@@ -1,15 +1,78 @@
 package com.quizztool.quizztoolbackend.services;
 
+import com.quizztool.quizztoolbackend.models.Alternative;
+import com.quizztool.quizztoolbackend.models.GradeType;
+import com.quizztool.quizztoolbackend.models.Question;
+import com.quizztool.quizztoolbackend.models.Quizz;
 import com.quizztool.quizztoolbackend.models.QuizzResult;
+import com.quizztool.quizztoolbackend.models.QuizzUser;
+import com.quizztool.quizztoolbackend.repositories.QuestionRepository;
+import com.quizztool.quizztoolbackend.repositories.QuizzRepository;
 import com.quizztool.quizztoolbackend.repositories.QuizzResultRepository;
 import java.util.List;
 
 public class QuizzResultService {
 
     QuizzResultRepository quizzResultRepository = new QuizzResultRepository();
-    
+
     public List<QuizzResult> getQuizzResult(int quizzId) {
         return quizzResultRepository.getQuizzResult(quizzId);
+    }
+
+    public QuizzResult submitAnswers(int userId, int quizzId, String answersRaw) {
+        QuestionRepository questionRepository = new QuestionRepository();
+        List<Question> questions = questionRepository.getQuestions(quizzId);
+
+        String answersSplitted[] = answersRaw.split(",");
+
+        int points = 0;
+        GradeType grade;
+
+        for (Question q : questions) {
+            for (Alternative a : q.getAlternatives()) {
+                if (a.isCorrect()) {
+                    for (String s : answersSplitted) {
+                        if (Integer.parseInt(s) == a.getAlternativeId()) {
+                            points++;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (points >= Math.floor(questions.size() * 0.75)) {
+            grade = GradeType.VG;
+        } else if (points >= Math.floor(questions.size() * 0.5)) {
+            grade = GradeType.G;
+        } else {
+            grade = GradeType.IG;
+        }
+
+        QuizzResult quizzResult = new QuizzResult();
+        quizzResult.setGrade(grade);
+        quizzResult.setPoints(points);
+        QuizzUser qu = new QuizzUser();
+        qu.setId(userId);
+        quizzResult.setQuizzUser(qu);
+        Quizz q = new Quizz();
+        q.setQuizzId(quizzId);
+        quizzResult.setQuizz(q);
+
+        quizzResultRepository.addResult(quizzResult);
+
+        System.out.println("Debaggor 1: " + points);
+        System.out.println("Debaggor 2: " + userId);
+
+        QuizzRepository quizzRepository = new QuizzRepository();
+        Quizz quizz = quizzRepository.getQuizz(quizzId);
+
+        System.out.println("Debaggor 3: " + quizz.isShowResult());
+
+        if (quizz.isShowResult()) {
+            return quizzResult;
+        } else {
+            return null;
+        }
     }
 
 }
